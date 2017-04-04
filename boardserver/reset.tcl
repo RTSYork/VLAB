48b68158376846776ef0c874cc8d8ed303f5971c
Russell Joyce <russell.joyce@york.ac.uk>
2017-04-04 18:09:23 +0100

    Updated board server device reset script The script should now clear non-Zynq FPGAs using a dummy bitfile

--- boardserver/reset.tcl
+++ boardserver/reset.tcl
@@ -1,3 +1,15 @@
 connect
-targets 1
-rst -system
+
+# Check if device is a Zynq
+if {[targets -filter {name =~ "APU"}] ne ""} {
+	# Reset Zynq SoC (also clears FPGA)
+	puts "Resetting Zynq and clearing FPGA..."
+	targets -set -filter {name =~ "APU"} -index 0
+	rst -system
+} else {
+	# Clear FPGA by attempting to program an invlaid bitstream
+	puts "Clearing FPGA..."
+	fpga reset.bin
+}
+
+disconnect
