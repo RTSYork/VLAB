51c37c3bc263691d056b06136cb9733c202b0b74
Russell Joyce <russell.joyce@york.ac.uk>
2020-04-14 19:33:46 +0100

    Run `dadutil enum` every minute on board hosts to check Digilent boards
    This should catch any boards that have been missed by 'dftdrvdtch' in
    the udev rules, and remove any rogue tty devices.
    
    Helps with #26.

--- host/opt/VLAB/boardscan.sh
+++ host/opt/VLAB/boardscan.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+DIR=/dev/vlab
+
 # Check the currently identified boards in /dev/vlab and if any of them
 # do not have their associated boardserver container running then create it.
 
-DIR=/dev/vlab
+if [[ -d "$DIR" ]]; then
+	# Force the Digilent utilities to update and enumerate boards
+	dadutil enum >> /dev/null
 
-if [ -d "$DIR" ]; then
 	for f in $DIR/*
 	do
 		serial=`basename $f`
