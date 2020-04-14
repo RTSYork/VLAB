f96b0d129663d5aed8238567d1232b45efe41672
Russell Joyce <russell.joyce@york.ac.uk>
2020-04-14 19:46:08 +0100

    Filtered out unwanted/erroneous firings of the board detach script
    Sometimes (e.g. when the Digilent utilities remove unneeded tty drivers)
    boards are 'detached' by udev when they are actually still running
    totally fine. The board detach script will now not run if all the device
    symlinks in /dev/vlab/ are present and resolve ok.
    
    The detach script also now does not run if the board's Docker container
    is not running (as this just caused an error further along anyway).
    
    Closes #26

--- host/opt/VLAB/boarddetach.sh
+++ host/opt/VLAB/boarddetach.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 serial=$1
+dev=/dev/vlab/$serial
+running=`docker inspect -f {{.State.Running}} cnt-$serial`
 
-/opt/VLAB/boarddetach.py $serial
+if [[ -e "$dev/jtag" && -L "$dev/jtag" && -a "$dev/jtag" && -e "$dev/tty" && -L "$dev/tty" && -a "$dev/tty" ]]; then
+	echo "Warning: Board $serial detach event fired but its device is still present, so ignoring."
+elif [[ "$running" != "true" ]]; then
+	echo "Warning: Board $serial detach event fired but its Docker container is not running, so ignoring."
+else
+	/opt/VLAB/boarddetach.py $serial
+fi
