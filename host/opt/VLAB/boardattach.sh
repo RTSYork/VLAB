d160181dd262e897320003f9f34429bffd45f5db
Russell Joyce <russell.joyce@york.ac.uk>
2020-04-14 19:37:02 +0100

    Switched board attach and detach scripts from using sh to bash

--- host/opt/VLAB/boardattach.sh
+++ host/opt/VLAB/boardattach.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 serial=$1
 running=`docker inspect -f {{.State.Running}} cnt-$serial`
 
-if [ "$running" != "true" ] ; then
-	/opt/VLAB/boardattach.py $serial
+if [[ "$running" == "true" ]]; then
+	echo "Warning: Board $serial attach event fired but its Docker container is already running, so ignoring."
 else
-	echo "Warning: Board $serial attach event fired but its Docker container is already running."
+	/opt/VLAB/boardattach.py $serial
 fi
